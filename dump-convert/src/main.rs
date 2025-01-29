1950900246572ba3b8ed015723896a6330af922d
ztereohype <57519662+ZtereoHYPE@users.noreply.github.com>
2025-01-30 00:05:25 +0100

    write converter of dump to PNG

---
+++ dump-convert/src/main.rs
@@ -0,0 +1,37 @@
+use std::env;
+use std::path::Path;
+use std::fs;
+use std::fs::File;
+use std::io::BufWriter;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Read user input
+    let args: Vec<String> = env::args().collect();
+    let path = &args[1];
+    let width = (&args)[2].parse::<u32>().expect("usage: filename width height");
+    let height = (&args)[3].parse::<u32>().expect("usage: filename width height");
+
+
+    // Read the dump file and convert the data
+    let mut data = fs::read(path)?;
+    for pixel in data.chunks_mut(4) {
+        let swap: u8 = pixel[0];
+        pixel[0] = pixel[2];
+        pixel[2] = swap;
+    }
+
+    // Encode as PNG
+    let path = Path::new(r"output.png");
+    let file = File::create(path).unwrap();
+    let ref mut w = BufWriter::new(file);
+    
+    let mut encoder = png::Encoder::new(w, width, height);
+    encoder.set_color(png::ColorType::Rgba);
+    encoder.set_depth(png::BitDepth::Eight);
+    encoder.set_source_gamma(png::ScaledFloat::new(0.5));
+    let mut writer = encoder.write_header().unwrap();
+    
+    writer.write_image_data(&data).unwrap();
+
+    Ok(())
+}
